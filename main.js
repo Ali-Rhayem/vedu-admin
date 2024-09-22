084ce912b65e7863ec76e003859e7be530a03bdf
Alirh1 <alirhayem.1212@gmail.com>
2024-09-22 11:21:45 +0300

    created main.js <> created main.js with launching dashboard as first page

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadFile('dashboard.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.on('navigate', (event, page) => {
+    if (page === 'dashboard') {
+        mainWindow.loadFile('dashboard.html');
+    } else if (page === 'users') {
+        mainWindow.loadFile('users.html');
+    } else if (page === 'courses') {
+        mainWindow.loadFile('courses.html');
+    }
+});
